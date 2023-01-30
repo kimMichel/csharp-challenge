097cfb068e87806da96f33f7a452aed0e9a35d0d
kim <kmichel.jae@gmail.com>
2023-01-29 21:40:44 -0300

    write and read line console; creating variable & data types

--- console/Program.cs
+++ console/Program.cs
@@ -1,4 +1,5 @@
-﻿public class Program
+﻿namespace console;
+public class Program
 {
     public static void Main(string[] args)
     {
---
+++ logica/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace logica
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("===============================");
+            Console.WriteLine("=Bem vindo ao primeiro sistema=");
+            Console.WriteLine("===============================");
+
+            Console.WriteLine("Digite o nome do primeiro colaborador:");
+            string? firstName = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do segundo colaborador:");
+            string? secondName = Console.ReadLine();
+
+            Console.WriteLine("Digite o nome do terceiro colaborador:");
+            string? thirdName = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome do primeiro colaborador:");
+            string? firstLastName = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome do segundo colaborador:");
+            string? secondLastName = Console.ReadLine();
+
+            Console.WriteLine("Digite o sobrenome do terceiro colaborador:");
+            string? thirdLastName = Console.ReadLine();
+
+            Console.WriteLine("===============================");
+
+            Console.WriteLine($"O primeiro colaborador se chama: {firstName + " " + firstLastName}");
+            Console.WriteLine($"O segundo colaborador se chama: {secondName + " " + secondLastName}");
+            Console.WriteLine($"O terceiro colaborador se chama: {thirdName + " " + thirdLastName}");
+
+            Console.WriteLine("===============[Finalizando o sistema]================");
+        }
+
+    }
+}
