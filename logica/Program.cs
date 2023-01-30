69c0d122bcdfaa914a3ef0a55bde19137da6b26e
kim <kmichel.jae@gmail.com>
2023-01-29 23:50:14 -0300

    c# conditional if, else if, else and switch

--- logica/Program.cs
+++ logica/Program.cs
@@ -8,24 +8,52 @@
             Console.WriteLine("=Bem vindo ao sistema=");
             Console.WriteLine("===============================");
 
-            Console.WriteLine("Quanto de combustivel tem disponivel? (L)");
-            double gasQuantity = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Qual o valor do litro da gasolina?");
-            double gasPrice = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("===============================");
-            Console.WriteLine("[Chegou um cliente]");
-            Console.WriteLine("===============================");
-            Console.WriteLine("Digite o nome do cliente:");
-            string? clientName = Console.ReadLine();
-            
-            Console.WriteLine("Quanto de gasolina o cliente deseja colocar em reais:");
-            double clientWillPay = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o primeiro número a ser calculado:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o segundo número a ser calculado:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("===========================================");
 
-            double gasInLiter = clientWillPay / gasPrice;
-            gasQuantity -= gasInLiter;
+            Console.WriteLine("Digite a opção desejada: 1 - Adição, 2 - Subtração, 3 - Multiplicação, 4 - Divisão");
+            int option = Convert.ToInt16(Console.ReadLine());
+            int total = 0;
 
-            Console.WriteLine($"O cliente {clientName} pagou R${clientWillPay} por {gasInLiter} L de gasolina.");
-            Console.WriteLine($"Resta {gasQuantity} L de gasolina no posto.");
+            // if (option == 1)
+            // {
+            //     total = firstNumber + secondNumber;
+            // }
+            // else if (option == 2)
+            // {
+            //     total = firstNumber - secondNumber;
+            // }
+            // else if (option == 3)
+            // {
+            //     total = firstNumber * secondNumber;
+            // } 
+            // else
+            // {
+            //     total = firstNumber / secondNumber;
+            // }
+
+            switch (option)
+            {
+                case 1:
+                    total = firstNumber + secondNumber;
+                    break;
+                case 2:
+                    total = firstNumber - secondNumber;
+                    break;
+                case 3:
+                    total = firstNumber * secondNumber;
+                    break;
+                case 4:
+                    total = firstNumber / secondNumber;
+                    break;
+            }
+
+
+
+            Console.WriteLine($"O total da operação é de {total}");
 
             Console.WriteLine("===============[Finalizando o sistema]================");
         }
