ad1fd432ceb93238c56aad6990cb5780b003d416
kim <kmichel.jae@gmail.com>
2023-01-30 01:31:07 -0300

    loop exercise

--- logica/Program.cs
+++ logica/Program.cs
@@ -8,52 +8,35 @@
             Console.WriteLine("=Bem vindo ao sistema=");
             Console.WriteLine("===============================");
 
-            Console.WriteLine("Digite o primeiro número a ser calculado:");
-            int firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Digite o segundo número a ser calculado:");
-            int secondNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("===========================================");
+            Console.WriteLine("Insira o nome do investidor:");
+            string? clientName = Console.ReadLine();
+            Console.WriteLine("Qual o valor que deseja depositar?");
+            double saldo = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Digite a opção desejada: 1 - Adição, 2 - Subtração, 3 - Multiplicação, 4 - Divisão");
-            int option = Convert.ToInt16(Console.ReadLine());
-            int total = 0;
+            Console.WriteLine("Deseja sacar? Sim(1) ou Não(2)");
+            int userAction = Convert.ToInt16(Console.ReadLine());
 
-            // if (option == 1)
-            // {
-            //     total = firstNumber + secondNumber;
-            // }
-            // else if (option == 2)
-            // {
-            //     total = firstNumber - secondNumber;
-            // }
-            // else if (option == 3)
-            // {
-            //     total = firstNumber * secondNumber;
-            // } 
-            // else
-            // {
-            //     total = firstNumber / secondNumber;
-            // }
-
-            switch (option)
+            double valorDoSaque = 0;
+            do
             {
-                case 1:
-                    total = firstNumber + secondNumber;
-                    break;
-                case 2:
-                    total = firstNumber - secondNumber;
-                    break;
-                case 3:
-                    total = firstNumber * secondNumber;
-                    break;
-                case 4:
-                    total = firstNumber / secondNumber;
-                    break;
-            }
+                valorDoSaque = 0;
+                switch (userAction)
+                {
+                    case 1:
+                        Console.WriteLine("Qual o valor que deseja sacar?");
+                        valorDoSaque += Convert.ToDouble(Console.ReadLine());
+                        break;
+                    case 2:
+                        break;
+                }
 
+                if (valorDoSaque > saldo)
+                {
+                    Console.WriteLine("O valor do saque é maior que o seu saldo.");
+                }
+            } while (valorDoSaque > saldo);
 
-
-            Console.WriteLine($"O total da operação é de {total}");
+            Console.WriteLine($"O valor sacado foi R$ {valorDoSaque} e o seu saldo é de R$ {saldo - valorDoSaque}");
 
             Console.WriteLine("===============[Finalizando o sistema]================");
         }
