eafc20fff484cc626f6d0aaf277cced219952823
kim <kmichel.jae@gmail.com>
2023-01-29 23:34:10 -0300

    exercice 2 convert toDouble

--- logica/Program.cs
+++ logica/Program.cs
@@ -5,35 +5,29 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("===============================");
-            Console.WriteLine("=Bem vindo ao primeiro sistema=");
+            Console.WriteLine("=Bem vindo ao sistema=");
             Console.WriteLine("===============================");
 
-            Console.WriteLine("Digite o nome do primeiro colaborador:");
-            string? firstName = Console.ReadLine();
-
-            Console.WriteLine("Digite o nome do segundo colaborador:");
-            string? secondName = Console.ReadLine();
-
-            Console.WriteLine("Digite o nome do terceiro colaborador:");
-            string? thirdName = Console.ReadLine();
-
-            Console.WriteLine("Digite o sobrenome do primeiro colaborador:");
-            string? firstLastName = Console.ReadLine();
-
-            Console.WriteLine("Digite o sobrenome do segundo colaborador:");
-            string? secondLastName = Console.ReadLine();
-
-            Console.WriteLine("Digite o sobrenome do terceiro colaborador:");
-            string? thirdLastName = Console.ReadLine();
-
+            Console.WriteLine("Quanto de combustivel tem disponivel? (L)");
+            double gasQuantity = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Qual o valor do litro da gasolina?");
+            double gasPrice = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("===============================");
+            Console.WriteLine("[Chegou um cliente]");
+            Console.WriteLine("===============================");
+            Console.WriteLine("Digite o nome do cliente:");
+            string? clientName = Console.ReadLine();
+            
+            Console.WriteLine("Quanto de gasolina o cliente deseja colocar em reais:");
+            double clientWillPay = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine($"O primeiro colaborador se chama: {firstName + " " + firstLastName}");
-            Console.WriteLine($"O segundo colaborador se chama: {secondName + " " + secondLastName}");
-            Console.WriteLine($"O terceiro colaborador se chama: {thirdName + " " + thirdLastName}");
+            double gasInLiter = clientWillPay / gasPrice;
+            gasQuantity -= gasInLiter;
+
+            Console.WriteLine($"O cliente {clientName} pagou R${clientWillPay} por {gasInLiter} L de gasolina.");
+            Console.WriteLine($"Resta {gasQuantity} L de gasolina no posto.");
 
             Console.WriteLine("===============[Finalizando o sistema]================");
         }
-
     }
 }
